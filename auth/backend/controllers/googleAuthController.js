cebda812e44f6a7d3f40b5ace3da06b8336f6a7a
thingsub <paularam0527@naver.com>
2025-10-01 05:28:06 +0000

    Fix : nginx에 home 추가 but 개선 필요

--- auth/backend/controllers/googleAuthController.js
+++ auth/backend/controllers/googleAuthController.js
@@ -112,13 +112,14 @@ exports.googleCallback = async (req, res) => {
 
     res.cookie("token", token, cookieOptions);
 
-    // 로그인 성공 후 리디렉션
-    const redirectUrl =
-      mappingResult.redirectUrl ||
-      process.env.FRONTEND_REDIRECT_URI ||
-      "http://localhost:3000/home";
+    // 로그인 성공 후 리디렉션(개발 단계의 별개 서버에서 동일한 도메인인 배포 단계로 넘어온 관계로 코드 수정 불가피)
+const frontendBaseUrl =
+  process.env.FRONTEND_REDIRECT_URI;
 
-    return res.redirect(redirectUrl);
+// const redirectUrl = `${frontendBaseUrl}/auth/google/callback`;
+
+// 리다이렉트 시 쿼리 파라미터로 code, state 같이 넘기기
+return res.redirect(`${frontendBaseUrl}/auth/home`);
   } catch (error) {
     console.error("Google Callback Error:", error);
     res.status(500).json({ success: false, message: "Google 로그인 실패" });
